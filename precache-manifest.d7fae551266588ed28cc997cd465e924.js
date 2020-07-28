64629b6bdd3859f3a6354be2abc38c05a99cb37e
Anish Vaidya <anishamulvaidya@gmail.com>
2020-07-27 23:15:08 -0700

    Updates

---
+++ precache-manifest.d7fae551266588ed28cc997cd465e924.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d882c150795382ccbd1e3bec72a8a46a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d9855316479a4888b1ed",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1cccfe8b5ae2315cb4c9",
+    "url": "/portfolio/static/css/main.5e4a77ab.chunk.css"
+  },
+  {
+    "revision": "d9855316479a4888b1ed",
+    "url": "/portfolio/static/js/2.e608954b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.e608954b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cccfe8b5ae2315cb4c9",
+    "url": "/portfolio/static/js/main.2d3030b1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d105153d3a416c6160dd5cc19e138a10",
+    "url": "/portfolio/static/media/logo.d105153d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7b3f0893b9d100b60b8b9062d9d75f6d.js"
+  "/portfolio/precache-manifest.d7fae551266588ed28cc997cd465e924.js"
 );
 
 self.addEventListener('message', (event) => {
